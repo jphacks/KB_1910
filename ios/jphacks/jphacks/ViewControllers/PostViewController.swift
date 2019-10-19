9498bdb0d1c4bb10218b2fb5c93986c735f45490
Utree <yuukis322+github@gmail.com>
2019-10-20 04:18:31 +0900

    投稿画面の作り込み

--- ios/jphacks/jphacks/ViewControllers/PostViewController.swift
+++ ios/jphacks/jphacks/ViewControllers/PostViewController.swift
@@ -10,13 +10,27 @@ import UIKit
 
 class PostViewController: UIViewController {
 
+    @IBOutlet weak var postImage: UIButton!
+    @IBOutlet weak var postTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func imageButton(_ sender: Any) {
+        print("tapped")
+    }
     
-
+    @IBAction func postButton(_ sender: Any) {
+    }
+    
+    /*
+     * テキスト入力時に他の場所を押したらキーボードを閉じる
+     */
+     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+         self.view.endEditing(true)
+     }
+    
     /*
     // MARK: - Navigation
 
