25d192483f76b04077499e706a560e68a3c3e77a
Utree <yuukis322+github@gmail.com>
2019-10-20 11:19:56 +0900

    tableview to collectionview

--- ios/jphacks/jphacks/ViewControllers/HomeViewController.swift
+++ ios/jphacks/jphacks/ViewControllers/HomeViewController.swift
@@ -7,21 +7,32 @@
 //
 
 import UIKit
+import Nuke
 
 class HomeViewController: UIViewController {
-    // tableView
-    @IBOutlet weak var tableView: UITableView!
-    
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // tableviewの設定
-        tableView.delegate = self
-        tableView.dataSource = self
-        // xibを登録
-        tableView.register(UINib(nibName: "CollectionView", bundle: nil), forCellReuseIdentifier: "collection")
 
         // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        // スクロールを禁止
+//        collectionView.isScrollEnabled = false
+        
+        // 横方向に並べていく
+        flowLayout.scrollDirection = .vertical
+        // 空白をなくす
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        // xibを登録
+        let inib = UINib(nibName: "CollectionItem", bundle: nil)
+        collectionView!.register(inib, forCellWithReuseIdentifier: "collectionItem")
+        let mnib = UINib(nibName: "MapCollectionViewCell", bundle: nil)
+        collectionView!.register(mnib, forCellWithReuseIdentifier: "mapCollectionItem")
     }
     
 
@@ -37,41 +48,53 @@ class HomeViewController: UIViewController {
 
 }
 
-extension HomeViewController: UITableViewDelegate ,UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        // セクション数
-        return 2
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    /*
+     * cellの個数
+     */
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
     }
+    
+    /*
+     * 見た目の設定
+     */
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.row == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mapCollectionItem", for: indexPath) as! MapCollectionViewCell
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // 各セクションごとのアイテム数
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            // 0番目: mapを表示
-            let cell = tableView.dequeueReusableCell(withIdentifier: "map") as! MapTableViewCell
+            
             return cell
         } else {
-            // 1番目: collectionViewを表示
-            let cell = tableView.dequeueReusableCell(withIdentifier: "collection") as! CollectionTableViewCell
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionItem", for: indexPath) as! ItemCollectionViewCell
+            
+            let array = ["https://images.unsplash.com/photo-1565706199223-96ec87a24026?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60","https://images.unsplash.com/photo-1559480671-4577ba1ea77b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60","https://images.unsplash.com/photo-1559480671-12ceff3e511d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60","https://images.unsplash.com/photo-1559480671-6c53c761ee1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60","https://images.unsplash.com/photo-1548712637-033b00ef9b4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60","https://images.unsplash.com/photo-1531055264461-e9e6b72744ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60","https://images.unsplash.com/photo-1503899036084-c55cdd92da26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60","https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60","https://images.unsplash.com/photo-1536098561742-ca998e48cbcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60","https://images.unsplash.com/photo-1513407030348-c983a97b98d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60","https://images.unsplash.com/photo-1498036882173-b41c28a8ba34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60","https://images.unsplash.com/photo-1554797589-7241bb691973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60","https://images.unsplash.com/photo-1551641506-ee5bf4cb45f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"]
+
+            let randomIndex = Int(arc4random_uniform(UInt32(array.count)))
+            
+            // 画像を設定
+            Nuke.loadImage(with: URL(string: array[randomIndex])!, into: cell.ImageItem)
             return cell
         }
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        // 縦幅
-        if indexPath.section == 0 {
-            return 500
+    /*
+     * 押下時の処理
+     */
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    
+    /*
+     * サイズを変更
+     */
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.row == 0 {
+            return CGSize(width: 400.0, height: 500.0)
         } else {
-            return 828
+            return CGSize(width: 207.0, height: 200.0)
         }
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Cell 押下時の処理
-    }
-
+    
 }
